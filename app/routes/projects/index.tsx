b3c2ebd7490c025bdbc8c7710c32a0b3c13c0c44
Tom Pham <thect04@gmail.com>
2022-08-25 16:40:52 +0700

    Add files via upload

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -12,92 +12,83 @@ import corkboard from "app/components/projects/corkboard.png";
 import spiritlabs from "app/components/projects/spiritlabs-card.png";
 
 export function links() {
-    return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: "stylesheet", href: styles }];
 }
 export default function Index() {
-    const [currentContent, setCurrentContent] = useState(true);
-    const [corkBoardSrc, setcorkBoardSrc] = useState(corkboardLow || corkboard);
+  const [currentContent, setCurrentContent] = useState(false);
+  const [corkBoardSrc, setcorkBoardSrc] = useState(corkboardLow || corkboard);
 
-    const scrollTo = (id: string) => {
-        const newContent = document.getElementById(id);
-        newContent !== null &&
-            newContent.scrollIntoView({
-                behavior: "smooth",
-            });
+  const scrollTo = (id: string) => {
+    const newContent = document.getElementById(id);
+    newContent !== null &&
+      newContent.scrollIntoView({
+        behavior: "smooth",
+      });
+  };
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = corkboard;
+    img.onload = () => {
+      setcorkBoardSrc(corkboard);
     };
+  }, []);
 
-    useEffect(() => {
-        const img = new Image();
-        img.src = corkboard;
-        img.onload = () => {
-            setcorkBoardSrc(corkboard);
-        };
-    }, []);
-
-    return (
-        <Wrapper className="container">
-            <span className="corkboard-wrapper">
-                <img
-                    className="corkboard"
-                    src={corkBoardSrc}
-                    alt="Memories Corkboard"
-                    width="100%"
-                />
-                <img
-                    className="spiritlabs"
-                    src={spiritlabs}
-                    alt="Spiritech Camp"
-                />
-            </span>
-            <button
-                id="view-change"
-                onClick={() => setCurrentContent(!currentContent)}
-            >
-                {currentContent
-                    ? "Toggle projects' notes"
-                    : "Toggle featured works"}
-            </button>
-            {currentContent ? (
-                <span className="details">
-                    <Cards />
-                </span>
-            ) : (
-                <span className="project-notes">
-                    <Notes />
-                </span>
-            )}
-            <button
-                id="view-change"
-                onClick={() => (
-                    setCurrentContent(!currentContent), scrollTo("view-change")
-                )}
-            >
-                {currentContent
-                    ? "View my projects' notes"
-                    : "View my featured works"}
-            </button>
-            <span className="verify">
-                <h3>
-                    Still in doubt? Ask{" "}
-                    <a href="https://www.reddit.com/user/lalonso2/">Raz</a> -
-                    leader of Yggdrasil clan and clan's discord owner for
-                    verification.
-                </h3>
-            </span>
-        </Wrapper>
-    );
+  return (
+    <Wrapper className="container">
+      <span className="corkboard-wrapper">
+        <img
+          className="corkboard"
+          src={corkBoardSrc}
+          alt="Memories Corkboard"
+          width="100%"
+        />
+        <img className="spiritlabs" src={spiritlabs} alt="Spiritech Camp" />
+      </span>
+      <button
+        id="view-change"
+        onClick={() => setCurrentContent(!currentContent)}
+      >
+        {currentContent ? "Toggle projects' notes" : "Toggle featured works"}
+      </button>
+      {currentContent ? (
+        <span className="details">
+          <Cards />
+        </span>
+      ) : (
+        <span className="project-notes">
+          <Notes />
+        </span>
+      )}
+      <button
+        id="view-change"
+        onClick={() => (
+          setCurrentContent(!currentContent), scrollTo("view-change")
+        )}
+      >
+        {currentContent ? "View my projects' notes" : "View my featured works"}
+      </button>
+      <span className="verify">
+        <h3>
+          Still in doubt? Ask{" "}
+          <a href="https://www.reddit.com/user/lalonso2/">Raz</a> - leader of
+          Yggdrasil clan and clan's discord owner for verification.
+        </h3>
+      </span>
+    </Wrapper>
+  );
 }
 
 export type projectCard = {
-    url: string;
-    type: string;
-    title: string;
-    order: number;
+  url: string;
+  type: string;
+  title: string;
+  order: number;
 };
 const Wrapper = styled.section`
-    max-width: 100vw;
-    display: flex;
-    flex-direction: column;
-    gap: 50px;
-    margin-top: 3px;
+  max-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  margin-top: 3px;
 `;
--- app/routes/projects/projectNotes.tsx
+++ app/routes/projects/projectNotes.tsx
@@ -10,30 +10,34 @@ import project7 from "app/components/projects/project7.png";
 import project8 from "app/components/projects/project8.png";
 import project9 from "app/components/projects/project9.png";
 import project10 from "app/components/projects/project10.png";
+import project11 from "app/components/projects/project11.png";
+import project12 from "app/components/projects/project12.png";
 
 export function links() {
-    return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: "stylesheet", href: styles }];
 }
 const Notes = () => {
-    return (
-        <span className="note-wrapper">
-            {projects.map((project, index) => (
-                <img key={index} src={project.url} alt={project.title} />
-            ))}
-        </span>
-    );
+  return (
+    <span className="note-wrapper">
+      {projects.map((project, index) => (
+        <img key={index} src={project.url} alt={project.title} />
+      ))}
+    </span>
+  );
 };
 const projects = [
-    { url: project1, title: "GovInsider" },
-    { url: project2, title: "Bartender Site" },
-    { url: project3, title: "Yggdrasil clan's avatar" },
-    { url: project4, title: "Discord stickers/emojis" },
-    { url: project5, title: "Marvel Future Fight gameplay" },
-    { url: project6, title: "Live Wallpapers" },
-    { url: project7, title: "Osu skin - Slim-eX" },
-    { url: project8, title: "AIP-BDET" },
-    { url: project9, title: "Yggdrasil's Google sheet" },
-    { url: project10, title: "Yggdrasil's OCR script" },
+  { url: project1, title: "GovInsider" },
+  { url: project2, title: "Bartender Site" },
+  { url: project12, title: "Online Portfolio" },
+  { url: project11, title: "Online Icebreaker Game" },
+  { url: project3, title: "Yggdrasil clan's avatar" },
+  { url: project4, title: "Discord stickers/emojis" },
+  { url: project5, title: "Marvel Future Fight gameplay" },
+  { url: project6, title: "Live Wallpapers" },
+  { url: project7, title: "Osu skin - Slim-eX" },
+  { url: project8, title: "AIP-BDET" },
+  { url: project9, title: "Yggdrasil's Google sheet" },
+  { url: project10, title: "Yggdrasil's OCR script" },
 ];
 
 export default Notes;
