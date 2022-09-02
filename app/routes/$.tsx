a3d7c8623c3c3065ab79038a70cb1e163de5c168
Tom Pham <thect04@gmail.com>
2022-09-02 11:33:27 +0700

    Add files via upload

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -2,19 +2,19 @@ import { Link } from "@remix-run/react";
 import styles from "app/styles/index.css";
 
 export function links() {
-    return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: "stylesheet", href: styles }];
 }
 
 export default function SplatRoute() {
-    return (
-        <div className="container">
-            <section id="splat">
-                <h3>Hmm, 404 Error?</h3>
-                <h3>This address seems weird. You sure it exists?</h3>
-                <h3>
-                    <Link to="/">Back to Home</Link>
-                </h3>
-            </section>
-        </div>
-    );
+  return (
+    <div className="container">
+      <section id="splat">
+        <h3>Hmm, 404 Error?</h3>
+        <h3>The URL seems weird. Are you sure it exists?</h3>
+        <h3>
+          <Link to="/">Back to Home</Link>
+        </h3>
+      </section>
+    </div>
+  );
 }
